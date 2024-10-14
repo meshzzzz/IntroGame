4d6e6b3a5e33e8c06c87d33dd3c4b207d6dc388d
ec22955 <annabel.s@me.com>
2024-10-14 15:26:52 +0100

    score & win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,9 +11,14 @@ public class PlayerController : MonoBehaviour
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 8;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
-    private void Start() {
+    void Start() {
         count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void OnMove(InputValue value) {
@@ -25,9 +32,17 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other) { 
-        if (other.gameObject.tag == "PickUp") {
-            count++;
+        if (other.gameObject.tag == "PickUp") {      
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText() {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups) {
+            winText.text = "You win!";
         }
     }
 }
