2ec84c132d72d64c35b32ebd2cd48dfeed0143b7
ec22955 <annabel.s@me.com>
2024-10-14 15:06:29 +0100

    pickups added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    private void Start() {
+        count = 0;
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) { 
+        if (other.gameObject.tag == "PickUp") {
+            count++;
+            other.gameObject.SetActive(false);
+        }
     }
 }
